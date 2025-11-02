b4fa3b8091739746a6b71ceb334bddb6547bbc17
jsomnium <jsomnium@naver.com>
2025-11-02 13:23:27 +0900

    chore(test): Lotto 클래스 import 경로 알맞게 수정

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,4 @@
-import Lotto from "../src/Lotto";
+import Lotto from "../src/model/Lotto.js";
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -7,14 +7,18 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
+  test("로또 번호의 개수가 6개 미만이면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5]);
+    }).toThrow("[ERROR]");
+  });
+
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
   test("로또 번호가 1~45 범위를 벗어나면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([0, 2, 3, 4, 5, 6]);
@@ -25,15 +29,16 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  test("올바른 로또 번호가 주어지면 정상적으로 생성된다.", () => {
-    const lotto = new Lotto([5, 12, 23, 34, 41, 45]);
-    expect(lotto.numbers).toEqual([5, 12, 23, 34, 41, 45]);
-  });
-
-  // 로또 번호에 숫자가 아닌 값이 들어오면 에러
   test("로또 번호에 숫자가 아닌 값이 들어오면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 'a', 6]);
     }).toThrow("[ERROR]");
   });
+
+  test("올바른 로또 번호가 주어지면 정상적으로 생성된다.", () => {
+    const lotto = new Lotto([5, 12, 23, 34, 41, 45]);
+    expect(lotto.getNumbers()).toEqual([5, 12, 23, 34, 41, 45]);
+  });
+
+  
 });
